8c28c0efa193b541326642116a378725d165a26f
CodeOfZaid <90457087+CodeOfZaid@users.noreply.github.com>
2021-10-12 12:18:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,39 +7,58 @@ function preload(){
 coinImg = loadImage("coin.png");
 bombImg = loadImage("bomb.png");
 energyDrinkImg = loadImage("energyDrink.png");
-powerimg = loadImage("power.png");
+powerImg = loadImage("power.png");
 
 }
 
 function setup(){
-  createCanvas(600,600);
+  createCanvas(400,600);
   //create sprites here
-path=createSprite(200,200);
+path=createSprite(200,300);
 path.addImage(pathImg);
 path.velocityY=5;
 path.scale=1.2;
-jake=createSprite(180,340,50,170);
+
+bomb=createSprite(100,50,50,170);
+bomb.addImage(bombImg);
+bomb.scale=0.2;
+
+energyDrink=createSprite(200,70,50,170);
+energyDrink.addImage(energyDrinkImg);
+energyDrink.scale=0.3;
+
+power=createSprite(300,60,50,170);
+power.addImage(powerImg);
+power.scale=0.2;
+
+jake=createSprite(180,460,50,170);
 jake.addAnimation("running",jakeImg);
+
 coin=createSprite(200,200,80,90);
 coin.addImage(coinImg);
 coin.scale=0.3;
+
 coin=createSprite(100,200,80,90);
 coin.addImage(coinImg);
 coin.scale=0.3;
+
 coin=createSprite(300,200,80,90);
 coin.addImage(coinImg);
 coin.scale=0.3;
+
 left_boundary=createSprite(0,300,100,600)
 right_boundary=createSprite(390,300,80,600)
 left_boundary.visible=false;
 right_boundary.visible=false;
+edges =createEdgeSprites();
 }
 
 function draw() {
-  background(0);
-path.velocityY=5
+  background("black");
+path.velocityY=5;
+if (keyDown("enter")) {
 jake.x=World.mouseX;
-
+}
 
 
 
@@ -53,5 +72,6 @@ jake.collide(left_boundary);
 jake.collide(right_boundary);
 edges=createEdgeSprites();
  jake.collide(edges[3]); 
-drawSprites;
+
+ drawSprites();
 }
